092428cb2257aab2932d5d087e130e5d23307844
pboachie <princeboachie@gmail.com>
2026-03-02 23:59:00 -0800

    fix: bypass django-tenants migrate override in a11y test setUpClass

--- tests/test_a11y_ci.py
+++ tests/test_a11y_ci.py
@@ -72,8 +72,11 @@ class AxeA11ySmokeTest(StaticLiveServerTestCase):
         super().setUpClass()
 
         from django.core.management import call_command
-        call_command("migrate", "--run-syncdb", verbosity=0)
-        call_command("migrate", "--database=audit", "--run-syncdb", verbosity=0)
+        from django.core.management.commands.migrate import Command as _DjangoMigrate
+        # Pass the Command class directly so django_tenants' migrate_schemas
+        # override (which requires set_schema, absent on SQLite) is bypassed.
+        call_command(_DjangoMigrate(), run_syncdb=True, verbosity=0)
+        call_command(_DjangoMigrate(), database="audit", run_syncdb=True, verbosity=0)
 
         cls._pw = sync_playwright().start()
         cls._browser = cls._pw.chromium.launch(headless=True)
